037d5e753cdade76bee575db8f3fe856ca8363b7
senioris <3rd.hierarchy@gmail.com>
2024-04-19 22:27:16 +0900

    in progress

--- lib/presentation/page/calender/calendar_page.dart
+++ lib/presentation/page/calender/calendar_page.dart
@@ -10,6 +10,8 @@ class CalendarPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final DateTime now = DateTime.now();
     final month = useState(now);
+    final day = useState(now);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -19,6 +21,7 @@ class CalendarPage extends HookConsumerWidget {
           icon: const Icon(Icons.arrow_back_ios_new_rounded),
           onPressed: () {
             month.value = DateTime(month.value.year, month.value.month - 1);
+            day.value = DateTime(month.value.year, month.value.month, 1);
           },
         ),
         actions: [
@@ -29,6 +32,7 @@ class CalendarPage extends HookConsumerWidget {
                       month.value.month < now.month) ||
                   (month.value.year < now.year)) {
                 month.value = DateTime(month.value.year, month.value.month + 1);
+                day.value = DateTime(month.value.year, month.value.month, 1);
               }
             },
           ),
@@ -37,7 +41,105 @@ class CalendarPage extends HookConsumerWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.check),
-      )
+      ),
+      body: Column(
+        children: [WeekCalendar(day: day)],
+      ),
+    );
+  }
+}
+
+class WeekCalendar extends HookConsumerWidget {
+  const WeekCalendar({
+    super.key,
+    required this.day,
+  });
+
+  final ValueNotifier<DateTime> day;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var startDay = _getSunday(day.value);
+
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          for (var i = 0; i < 7; i++)
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.only(left: 5, right: 5),
+              child: Container(
+                  decoration: i == 0
+                      ? BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white,
+                          boxShadow: const [
+                              BoxShadow(
+                                color: Color.fromARGB(255, 223, 217, 217),
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                offset:
+                                    Offset(1, 1), // changes position of shadow
+                              ),
+                            ])
+                      : null,
+                  child: DayView(day: startDay.add(Duration(days: i)))),
+            )),
+        ],
+      ),
+    );
+  }
+
+  DateTime _getSunday(DateTime date) {
+    return date.subtract(Duration(days: date.weekday));
+  }
+}
+
+class DayView extends HookConsumerWidget {
+  const DayView({
+    super.key,
+    required this.day,
+  });
+
+  final DateTime day;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return InkWell(
+      customBorder:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      onTap: () => {},
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            DateFormat(DateFormat.ABBR_WEEKDAY).format(day),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            DateFormat(DateFormat.DAY).format(day),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Container(
+            height: 10,
+            width: 10,
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(0xd4, 0xfd, 0xa2, 1),
+              shape: BoxShape.circle,
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+        ],
+      ),
     );
   }
 }
